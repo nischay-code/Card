f56ad28e11701598953585ae57ef31aec189ccb6
Nischay Patel <95124327+nischay-code@users.noreply.github.com>
2023-11-27 10:04:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("sorryCard").addEventListener("click", function () {
+  this.innerHTML = `
+      <div class="card-content">
+        <h2>Thank You!</h2>
+        <p>I am truly blessed to have you in my life. Your presence brings joy to my soul and warmth to my
+        heart.</p>
+      </div>
+    `;
+});
