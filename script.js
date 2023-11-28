9933ce5c03162e403024acf6ee1760de5ebef582
Nischay Patel <95124327+nischay-code@users.noreply.github.com>
2023-11-28 09:08:27 +0530

    Content

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ document.getElementById("sorryCard").addEventListener("click", function () {
   this.innerHTML = `
       <div class="card-content">
         <h2>Thank You!</h2>
-        <p>I am truly blessed to have you in my life. Your presence brings joy to my soul and warmth to my
-        heart.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing dolor sit amet consectetur.</p>
       </div>
     `;
 });
